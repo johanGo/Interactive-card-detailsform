f1e90267a696917fb64aa5bd823c817e5e896e5c
johanGo <108165617+johanGo@users.noreply.github.com>
2025-01-09 22:26:33 -0500

    Add files via upload
    Files uploaded

--- script.js
+++ script.js
@@ -1,71 +1,91 @@
-//seccion del holdername
-let nameCard = document.querySelector('.card-name');
-let nameInput = document.querySelector('#cardholder');
-let nameError = document.querySelector('.cardholder-error')
+let containerCountriess = document.getElementById("containerCountries");
+let containerFilterRegions = document.getElementById("filterRegions");
+let containerSearchCountry = document.getElementById("searchCountry");
+let allRegions = [];
+let uniqueRegions;
+let yaSeEjecutó = false;
 
-// seccion del card number
-let cardNumber = document.querySelector('.card-numbers');
-let numberInput = document.querySelector('#cardNumber');
-let numberError = document.querySelector('.cardNumber-error');
-
-//seccion month
-let cardMonth = document.querySelector('.card-month');
-let monthInput= document.getElementById('cardMonth');
-let monthError = document.querySelector('.month-error');
-
-
-//ingreso dinamico del name
-nameInput.addEventListener('input', ()=>{
-    if(nameInput.value=== ''){
-        nameCard.innerText="JANE APPLESEED";
-    }else{
-        nameCard.innerText=nameInput.value;
+containerFilterRegions.addEventListener('change', () => {
+  if (containerFilterRegions.value == 'default') {
+    cargarPaises();
+  } else {
+    function cargarPaisesRegion() {
+      fetch(`https://restcountries.com/v3.1/region/${containerFilterRegions.value}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          let countryHTML = '';
+          data.forEach(item => {
+            countryHTML += `
+                          <div class="firstCountry">
+                            <img class="imageCountry" src="${item.flags.png}" alt="">
+                            <div class="containerDataCountry">
+                              <span class="nameCountry">${item.name.common}</span>
+                              <div class="populationCountry">Population: <span>${item.population}</span></div>
+                              <div class="regionCountry">Region: <span>${item.region}</span></div>
+                              <div class="capitalCountry">Capital: <span>${item.capital}</span></div>
+                            </div>
+                          </div>
+                          `;
+            containerCountriess.innerHTML = countryHTML;
+          });
+        })
+        .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
     }
+    cargarPaisesRegion();
+  }
 })
 
-//ingreso dinamico del number
-numberInput.addEventListener('input', event=>{
-    let inputValue = event.target.value;
-
-    //Actualizando graficamente la tarjeta
-    cardNumber.innerText=numberInput.value;
-
-    let regExp=/[A-z]/g;
-    if(regExp.test(numberInput.value)){
-        showError(numberInput,numberError, "Wrong format, numbers only" );        
-    }else{
-        numberInput.value=inputValue.replace(/\s/g, '').replace(/([0-9]{4})/g, '$1 ').trim();
-        hideError(numberError, numberInput);        
-    }
-    
-    if(numberInput.value ==''){
-        cardNumber.innerText="0000 0000 0000 0000"
-    }
-});
-
-//ingreso dinamico del mes
-monthInput.addEventListener('input', ()=>{
-    let regExp2=/[A-z]/g;
-    if(monthInput.value>=13 || regExp2.test(monthInput.value)){
-        showError(monthInput, monthError, 'Wrong format')
-    }else{
-        hideError(monthError, monthInput);
-    }
-    if(monthInput.value ==''){
-        cardMonth.innerText='00';
-    }else{
-        cardMonth.innerText=monthInput.value;
-    }
-});
-
-//funciones
-function showError(divInput, divError, messageError){
-    divError.innerText=messageError;
-    divInput.style.borderColor='red'
-};
-
-function hideError(divError,divInput){
-    divError.innerText='';
-    divInput.style.borderColor='';
+function cargarPaises() {
+  fetch(`https://restcountries.com/v3.1/name/deutschland`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      let countryHTML = '';
+      data.forEach(item => {
+        countryHTML += `
+                      <div class="firstCountry">
+                        <img class="imageCountry" src="${item.flags.png}" alt="">
+                        <div class="containerDataCountry">
+                          <span class="nameCountry">${item.name.common}</span>
+                          <div class="populationCountry">Population: <span>${item.population}</span></div>
+                          <div class="regionCountry">Region: <span>${item.region}</span></div>
+                          <div class="capitalCountry">Capital: <span>${item.capital}</span></div>
+                        </div>
+                      </div>
+                      `;
+        containerCountriess.innerHTML = countryHTML;
+        allRegions.push(item.region)   //añade las regiones al array regions
+      });
+      if (!yaSeEjecutó) {
+        uniqueRegions = [...new Set(allRegions)] // array que contiene solo la unica region
+        console.log(uniqueRegions)
+        uniqueRegions.forEach(item => {
+          let tagOption = document.createElement('option');
+          tagOption.setAttribute('value', item);
+          tagOption.innerHTML = item;
+          containerFilterRegions.appendChild(tagOption)
+          yaSeEjecutó = true;
+        })
+      }
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 }
+cargarPaises();
+
+containerSearchCountry.addEventListener('input',(event)=>{
+  console.log(event.target.value)
+})
 
